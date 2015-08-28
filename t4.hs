8dede264807817fd87dd103e24bbb1dfcd7ffc3a
Luis Henrique Medeiros <lhmedeiros0@gmail.com>
2015-08-28 18:04:21 -0300

    T4
    Exercícios T4

---
+++ t4.hs
@@ -0,0 +1,32 @@
+--1
+elevaQuad :: [Int] -> [Int]
+elevaQuad [] = []
+elevaQuad (x:xs) = x ^ 2 : elevaQuad xs
+
+--2
+containChar :: Char -> String -> Bool
+containChar _ [] = False
+containChar c (x:xs)
+	| x == c = True
+	| x /= c = containChar c xs
+
+--3
+semVogais :: String -> String
+semVogais [] = []
+semVogais (x:xs)
+	| (x == 'a') || (x == 'e') || (x == 'i') || (x == 'o') || (x == 'u') = semVogais xs
+	| otherwise = x : semVogais xs
+
+--4
+translate :: [Float] -> [(Float, Float)]
+translate [] = []
+translate (x:xs) = (x, head xs) : translate (tail xs)
+
+--5
+geraTabela :: Int -> [(Int, Int)]
+geraTabela x = aux 1 x
+
+aux :: Int -> Int -> [(Int, Int)]
+aux count x
+	| count < x + 1 = (count, count ^ 2) : aux (count + 1) x
+	| otherwise = []
