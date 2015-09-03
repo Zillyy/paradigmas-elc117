476ad9858df537f172909a7d020073b4a48f623c
Luis Henrique Medeiros <lhmedeiros0@gmail.com>
2015-09-03 11:14:19 -0300

    T5
    Ex 3 adicionado

--- t5.hs
+++ t5.hs
@@ -6,3 +6,10 @@ addSuffix suffix str = [x ++ suffix | x <- str]
 addSuffix' :: String -> [String] -> [String]
 addSuffix' _ [] = []
 addSuffix' suffix (x:xs) = (x ++ suffix) : addSuffix suffix xs 
+
+--3
+countShorts :: [String] -> Int
+countShorts [] = 0
+countShorts (x:xs)
+	| length x < 5 = 1 + countShorts xs
+	| otherwise = countShorts xs
