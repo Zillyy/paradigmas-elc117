f87145422ddc7a6225c1d8af8677ee56bc4f1ca1
Luis Henrique Medeiros <lhmedeiros0@gmail.com>
2015-09-03 11:07:25 -0300

    T5
    Ex 2 adicionado

--- t5.hs
+++ t5.hs
@@ -1,3 +1,8 @@
 --1
 addSuffix :: String -> [String] -> [String]
 addSuffix suffix str = [x ++ suffix | x <- str]
+
+--2
+addSuffix' :: String -> [String] -> [String]
+addSuffix' _ [] = []
+addSuffix' suffix (x:xs) = (x ++ suffix) : addSuffix suffix xs 
