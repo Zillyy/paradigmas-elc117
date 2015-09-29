3974c52a6949c4b5fb8043aebb240a88c24cc98d
Luis Henrique Medeiros <lhmedeiros0@gmail.com>
2015-09-28 23:05:15 -0300

    Ex 1 a 4 prontos

--- prolog/t7.pl
+++ prolog/t7.pl
@@ -2,5 +2,16 @@
 A função verifica se o elemento da segunda lista é igual ao de mesmo
 índice*/
 
+% Ex 2
+ziplus([], [], []).
+ziplus([H|T], [H1|T1], [H2|T2]) :- H2 is H + H1, ziplus(T, T1, T2).  
+
+% Ex 3
 countdown(-1, []).
 countdown(N, [H|T]) :- H is N, N1 is N - 1, countdown(N1, T).
+
+% Ex 4
+potencias(N, L) :- C is 0, potenciasAux(C, N, L).
+
+potenciasAux(_, 0, []).
+potenciasAux(C, N, [H|T]) :- H is 2 ^ C, N1 is N - 1, C1 is C + 1, potenciasAux(C1, N1, T).
