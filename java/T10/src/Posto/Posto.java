7d7d0deab8c3ec29e8fd1974b86e45894e6c2ebd
Luis Henrique Medeiros <lhmedeiros0@gmail.com>
2015-11-16 19:55:56 -0200

    Classe Posto adicionada no T10

---
+++ java/T10/src/Posto/Posto.java
@@ -0,0 +1,104 @@
+package Posto;
+
+/* @author Zilly
+ */
+public class Posto {
+
+    protected int cnpj;
+    protected String razaoSocial;
+    protected String nomeFantasia;
+    protected String bandeira;
+    protected String endereco;
+    protected String bairro;
+    protected int cep;
+    protected String imagem;
+
+    //Construtor padrão
+    public Posto() {
+        this.cnpj = 0;
+        this.razaoSocial = "";
+        this.nomeFantasia = "";
+        this.bandeira = "";
+        this.endereco = "";
+        this.bairro = "";
+        this.cep = 0;
+        this.imagem = "";
+    }
+
+    //Construtor alternativo
+    public Posto(int cnpj, String razaoSocial, String nomeFantasia, String bandeira, String endereco, String bairro, int cep, String imagem) {
+        this.cnpj = cnpj;
+        this.razaoSocial = razaoSocial;
+        this.nomeFantasia = nomeFantasia;
+        this.bandeira = bandeira;
+        this.endereco = endereco;
+        this.bairro = bairro;
+        this.cep = cep;
+        this.imagem = imagem;
+    }
+
+    //Getters e Setters
+    public int getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(int cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public String getRazaoSocial() {
+        return razaoSocial;
+    }
+
+    public void setRazaoSocial(String razaoSocial) {
+        this.razaoSocial = razaoSocial;
+    }
+
+    public String getNomeFantasia() {
+        return nomeFantasia;
+    }
+
+    public void setNomeFantasia(String nomeFantasia) {
+        this.nomeFantasia = nomeFantasia;
+    }
+
+    public String getBandeira() {
+        return bandeira;
+    }
+
+    public void setBandeira(String bandeira) {
+        this.bandeira = bandeira;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public int getCep() {
+        return cep;
+    }
+
+    public void setCep(int cep) {
+        this.cep = cep;
+    }
+
+    public String getImagem() {
+        return imagem;
+    }
+
+    public void setImagem(String imagem) {
+        this.imagem = imagem;
+    }
+}
